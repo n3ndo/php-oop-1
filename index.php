7a9e36b91b7a5a4c4f2c67a49767509e409c1adf
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-01-30 13:39:03 +0100

    [UPDATE] exercise done

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
-    
+    class Movie {
+        public $title;
+        public $age_rating;
+        public $genre;
+
+        function __construct($_title, $_age_rating, $_genre){
+            $this->title = $_title;
+            $this->age_rating = $_age_rating;
+            $this->genre = $_genre;
+        }
+
+        public function isRatedPG(){
+            if($this->age_rating == "PG"){
+              return true;
+            }else{
+              return false;
+            }
+          }
+
+    }
+
+    $movie1 = new Movie("Harry Potter", "PG", "Fantasy");
+    $movie2 = new Movie("Lord of the Rings", "PG-13", "Fantasy");
+
+    if($movie1->isRatedPG()){
+        echo "<div>".$movie1->title." è un film classificato PG."."<br>".$movie1->genre."</div>";
+      }else{
+        echo "<div>".$movie1->title."non è un film classificato PG."."<br>".$movie1->genre."</div>";
+      }
+
+    if($movie2->isRatedPG()){
+        echo "<div>".$movie2->title." è un film classificato PG."."<br>".$movie2->genre."</div>";
+      }else{
+        echo "<div>".$movie2->title." non è un film classificato PG."."<br>".$movie2->genre."</div>";
+      }
 ?>
